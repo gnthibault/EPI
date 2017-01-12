f44ab78e620daeb47df09864029577429b876e54
Thibault Notargiacomo <gnthibault@gmail.com>
2017-01-13 00:38:52 +0100

    Fixed Quick Sort

--- Sort/QuickSort.cpp
+++ Sort/QuickSort.cpp
@@ -7,32 +7,34 @@
 #include <iostream>
 #include <cassert>
 
-//In place recursive algorithm for O(n log2(n)) sort
-template<typename T>
-std::vector<T> QuickSort(std::vector<T> in, size_t begin, size_t end) {
-  std::vector<T> v = in;
-  assert(begin<=end);
-  size_t s = end-begin;
-  if (s>1) {
-    T piv = v.at(begin);
-    size_t pivIdx = begin;
-    for (size_t i=begin+1; i<end;i++) {
-      if (v.at(i)<piv) {
-        //std::swap(v[i],v[pivIdx]);
-        pivIdx++;
-      }
-      v=QuickSort(v,begin,pivIdx);
-      v=QuickSort(v,pivIdx,end);
-    }
-  }
-  return v;
-}
-
 template<typename T>
 void Print(const std::vector<T>& v) {
   std::for_each( v.cbegin(), v.cend(), [](T in) {
     std::cout << in << ","; } );
   std::cout << std::endl;
+}
+
+//In place recursive algorithm for O(n log2(n)) sort
+template<typename T>
+std::vector<T> QuickSort(std::vector<T> in, size_t begin, size_t end) {
+  std::vector<T> v(in);
+  assert(begin<=end);
+  size_t size = end-begin;
+  if (size>1) {
+    T pivIdx = begin+size/2;//could use rand(begin,end)
+    std::swap(v[pivIdx],v[end-1]);
+    size_t j=begin;
+    for (size_t i=begin; i<end-1; i++) {
+      if (v[i]<=v[end-1]) {
+        std::swap(v[i],v[j]);
+        j++;
+      }
+    }
+    std::swap(v[j],v[end-1]);
+    v=QuickSort(v,begin,j);
+    v=QuickSort(v,j+1,end);
+  }
+  return v;
 }
 
 int main(int argc, char* argv[])
