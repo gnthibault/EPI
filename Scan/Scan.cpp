4713d4b865650dc4c2957538d1cd85e0415ed7be
Thibault Notargiacomo <gnthibault@gmail.com>
2017-01-14 18:36:37 +0100

    Corrected Hillis scan

--- Scan/Scan.cpp
+++ Scan/Scan.cpp
@@ -28,7 +28,8 @@ std::vector<T> SequentialInclusiveScan(const std::vector<T>& v) {
 }
 
 /*
-* Hillis & Steele Parallel scan
+* Hillis & Steele Parallel scan, input vector size SHOULD be a power of 2
+* It has a step complexity of O(log2(n)), but a work complexity of O(n log(n))
 */
 template<template<typename U> class Op, typename T>
 std::vector<T> HillisSteelInclusiveScan(const std::vector<T>& v) {
@@ -38,8 +39,34 @@ std::vector<T> HillisSteelInclusiveScan(const std::vector<T>& v) {
   for(size_t n = 1; n<ret.size();n*=2) {
     std::swap(ret,tmp);
     #pragma omp parallel for
-    for(size_t i=n; i<ret.size(); i++) {
-      ret[i]=Op<T>::Op(tmp[i],tmp[i-n]);
+    for (size_t i=n/2; i<ret.size(); i++) {
+      if (i<n) {
+        ret[i] = tmp[i];
+      } else {
+        ret[i]=Op<T>::Op(tmp[i],tmp[i-n]);
+      }
+    }
+  }
+  return ret;
+}
+
+/*
+* Blelloch Parallel scan, input vector size SHOULD be a power of 2
+* It has a step complexity of O(log2(n)), but a work complexity of O(n log(n))
+* By design Blelloch scan is an exclusive scan, so we did a simple modification
+* at the end to that we return the inclusive scan version
+*/
+template<template<typename U> class Op, typename T>
+std::vector<T> BlellochInclusiveScan(const std::vector<T>& v) {
+  assert(v.size()>0);
+
+  // The reduce part
+  std::vector<T> ret(v),tmp(v.size());
+  for(size_t n = 1; n<ret.size();n*=2) {
+    std::swap(ret,tmp);
+    #pragma omp parallel for
+    for(size_t i=0; i<ret.size()/(2*n); i++) {
+      ret[2*i+n]=Op<T>::Op(tmp[2*i],tmp[2*i+n]);
     }
     Print(ret);
   }
