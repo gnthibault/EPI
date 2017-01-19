af6969170aa9e4e766f248013e92a7156fff8dab
Thibault Notargiacomo <gnthibault@gmail.com>
2017-01-19 23:46:09 +0100

    attempt at k largest

--- ch12/kthLargest.cpp
+++ ch12/kthLargest.cpp
@@ -2,11 +2,38 @@
 
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 template<typename T>
 T GetKthLargest(std::vector<T> v, int k) {
+  //The idea will be to perform a low cost sorting with a pivot
+  assert(k<=v.size());
+  assert(k>0);
 
-  return v[0];
+  int64_t start=0,stop=v.size()-1,pivIdx=0;
+  while (pivIdx!=k) {
+    if (pivIdx>k) {
+      pivIdx--; //or random position
+      stop=pivIdx;
+    } else {
+      start = pivIdx;
+    }
+    T pivot = v[pivIdx];
+    std::swap(v[pivIdx],v[stop]);
+    //Elements <= v[idx] will be inserted before v[idx]
+    int64_t i=start,j=stop;
+    while (i<j) {
+      if (v[j]<pivot) {
+        std::swap(v[j],v[i]);
+        i++;
+      } else {
+        j--;
+      }
+      //All elements <=pivot are before i index (excluded)
+      pivIdx = i;
+    }
+  }
+  return v[pivIdx-1];
 }
 
 int main (int argc, char* argv[]) {
@@ -14,6 +41,7 @@ int main (int argc, char* argv[]) {
   std::cout << "1 Largest elem is " << GetKthLargest(v,1) << std::endl;
   std::cout << "2 Largest elem is " << GetKthLargest(v,2) << std::endl;
   std::cout << "3 Largest elem is " << GetKthLargest(v,3) << std::endl;
+  std::cout << "4 Largest elem is " << GetKthLargest(v,4) << std::endl;
 
   return EXIT_SUCCESS;
 }
